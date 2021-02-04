cbf7079d6acff058b2c68d49df0f25ed2092a3ae
Exophobias <exophobias@outlook.com>
2021-02-04 00:03:51 -0800

    Connect to database

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // Parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+// Configuring the database
+const dbConfig = require('./config/db.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
 // Defines a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to Simple Checkers. Play a round or two!"});
