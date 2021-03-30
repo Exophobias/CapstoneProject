65c1c9767bcb6fef3679558a14e64857bc059e34
Exophobias <exophobias@outlook.com>
2021-03-30 13:26:14 -0700

    Made entire program to load on window load

--- app/main.js
+++ app/main.js
@@ -1,37 +1,52 @@
 
-// Initialize gameboard
-var board = [
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0],
-    [0, 1, 0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 0, 2, 0, 2, 0, 2, 0],
-    [0, 2, 0, 2, 0, 2, 0, 2],
-    [2, 0, 2, 0, 2, 0, 2, 0]
-  ];
+// Load program on web browser load
+window.onload = function() {
+    // Gameboard design. Every value > 0 = a checker piece and its respective ID
+    var gameBoard = [
+        [0, 1, 0, 2, 0, 3, 0, 4],
+        [5, 0, 6, 0, 7, 0, 8, 0],
+        [0, 9, 0, 10, 0, 11, 0, 12],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [13, 0, 14, 0, 15, 0, 16, 0],
+        [0, 17, 0, 18, 0, 19, 0, 20],
+        [21, 0, 22, 0, 23, 0, 24, 0]
+    ];
 
-// 
-var pieces = [];
-var tiles = [];
+    // 
+    var pieces = [];
+    var tiles = [];
 
-// Piece Object - 24 total pieces in the game (12 per side)
-function Piece(element, position) {
+    // Piece Object - 24 total pieces (instances) in the game (12 per side)
+    // Includes functions for possible/valid moves, makeKing, etc.
+    function Piece(element, position) {
 
-    // Linked DOM (HTML) Element
-    this.element = element;
+        // Linked DOM (HTML) Element
+        //this.element = element;
 
-    // Gameboard is a 2-D array. Format is row #, then column position. Ex. [0][1]
-    this.position = position;
+        // Gameboard is a 2-D array. Format is row #, then column position. Ex. [0][1]
+        this.position = position;
 
-    // Which player's piece?
-    this.player = '';
+        // Which player's piece?
+        this.player = '';
 
-    // Assigns HTML elements (checker pieces) based on HTML element ID
-    if(this.element.attr("id") < 12)
-        this.player = 1;
-    else
-        this.player = 2;
+        // Is the piece a king?
+        this.isKing = false;
 
-    
+        // Assigns HTML elements (checker pieces) based on HTML element ID
+        if(this.element.attr("id") < 13)
+            this.player = 1;
+        else
+            this.player = 2;
+
+        function move(tile) {
+
+        }
+
+
+    }
+
+    function Board() {
+
+    }
 }
