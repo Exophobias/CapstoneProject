dd43ec17e6f9b96a7e941621f52916a5ec44e310
Exophobias <exophobias@outlook.com>
2021-03-30 18:28:20 -0700

    Update main.js
    Added render functions, check functions

--- app/main.js
+++ app/main.js
@@ -22,13 +22,16 @@ window.onload = function() {
     function Piece(element, position) {
 
         // Linked DOM (HTML) Element
-        //this.element = element;
+        this.element = element;
 
         // Gameboard is a 2-D array. Format is row #, then column position. Ex. [0][1]
         this.position = position;
 
         // Which player's piece?
         this.player = '';
+
+        // On initializaiton, all pieces are allowed to move as there is no jump
+        this.allowedToMove = true;
 
         // Is the piece a king?
         this.isKing = false;
@@ -41,12 +44,60 @@ window.onload = function() {
 
         function move(tile) {
 
+
         }
 
 
     }
 
+    // Controls the logistics and of the game
     function Board() {
+
+        board: gameBoard // Maps board to gameBoard object
+        score: {
+            player1: 0
+            player2: 0
+        }
+        playerTurn = 1;
+        jumpexist = false;
+        tilesElement = $('div.tiles');
+        dictionary: ["0vmin", "10vmin", "20vmin", "30vmin", "40vmin", "50vmin", "60vmin", "70vmin", "80vmin", "90vmin"],
+
+        // This populates the elements within the index.html 
+        function tileRender(row, column, countTiles) {
+            this.tilesElement.append("<div class='tile' id='tile" + countTiles + "' style='top:" + this.dictionary[row] + ";left:" + this.dictionary[column] + ";'></div>");
+            tiles[countTiles] = new Tile($("#tile" + countTiles), [parseInt(row), parseInt(column)]);
+            return countTiles + 1
+        }
+
+        // 
+        function renderPlayerPieces(playerNumber, row, column, countPieces) {
+            $(`.player${playerNumber}pieces`).append("<div class='piece' id='" + countPieces + "' style='top:" + this.dictionary[row] + ";left:" + this.dictionary[column] + ";'></div>");
+            pieces[countPieces] = new Piece($("#" + countPieces), [parseInt(row), parseInt(column)]);
+            return countPieces + 1;
+          }
+
+        // Checks if location (tile) has an object (piece) in that location
+        function isValidMove(row, column) {
+            // Check if row and column is within boundss of the gameboard
+            if (row < 0 || row > 7 || column < 0 || column > 7)
+                return false;
+            
+            // If location in 2D array == 0, then no piece is there. Valid move
+            if (this.board[row][column] == 0)
+                return true;
+            return false;
+        }
+
+        function changePlayerTurn() {
+            if (this.playerTurn == 1) {
+                this.playerTurn = 2
+            } else {
+                this.playerTurn = 1
+            }
+        }
+
+
 
     }
 }
